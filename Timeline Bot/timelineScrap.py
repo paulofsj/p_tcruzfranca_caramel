7ac727d2d7b688ec9b930057877d8409507375cd
paulofsj <paulo2012@gmail.com>
2020-04-14 16:39:21 -0300

    Update timelineScrap.py
    Alterações para funcionar com o Docker.

--- Timeline Bot/timelineScrap.py
+++ Timeline Bot/timelineScrap.py
@@ -36,7 +36,13 @@ def extraindoApenasTexto(tweet_texto):
 
 def getDriverOnTwitter(timeout):
     # create a new browser session
-    driver = webdriver.Chrome("./chromedriver")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(executable_path="./chromedriver",chrome_options=chrome_options)
     driver.implicitly_wait(timeout)#30 seconds to launch the browser
     driver.maximize_window()#When launched, maximizer it
     # Navigate to the application home page
